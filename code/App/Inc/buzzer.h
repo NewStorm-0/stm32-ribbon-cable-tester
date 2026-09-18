5a8cb13161bec3301842cbe71dd9a666cb54389c
NewStorm <2456601397@qq.com>
2026-09-18 22:28:34 +0800

    feat: add buzzer driver

---
+++ code/App/Inc/buzzer.h
@@ -0,0 +1,9 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+#include <stdint.h>
+
+void Buzzer_On(void);
+void Buzzer_Off(void);
+
+#endif /* BUZZER_H */
---
+++ code/App/Src/buzzer.c
@@ -0,0 +1,12 @@
+#include "buzzer.h"
+#include "main.h"
+
+void Buzzer_On(void)
+{
+    HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, GPIO_PIN_SET);
+}
+
+void Buzzer_Off(void)
+{
+    HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, GPIO_PIN_RESET);
+}
